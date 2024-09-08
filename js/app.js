40f2170600ee99c9150be397204da472c073d0e1
Tino Sabetta <tino.sabetta@gmail.com>
2024-09-08 13:01:18 +0200

    Add offline capability with service worker and caching

--- js/app.js
+++ js/app.js
@@ -159,3 +159,14 @@ function init() {
 }
 
 init();
+
+// Service Worker registration
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+            console.log('Service Worker registered with scope:', registration.scope);
+        }).catch(function(error) {
+            console.log('Service Worker registration failed:', error);
+        });
+    });
+}
